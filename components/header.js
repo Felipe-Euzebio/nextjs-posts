bcd98312c6c7c42ebdfce7c2b6e8c7d5bcee09a2
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-06-05 22:40:16 -0300

    Controlling the Logo Image Size

--- components/header.js
+++ components/header.js
@@ -6,7 +6,12 @@ export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <Image src={logo} alt="Mobile phone with posts feed on it" />
+        <Image 
+          src={logo} 
+          alt="Mobile phone with posts feed on it" 
+          width={100}
+          height={100}
+        />
       </Link>
       <nav>
         <ul>
@@ -14,10 +19,13 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
     </header>
   );
 }
+
